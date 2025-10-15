c00cbd0f2c950237dc19e17025774c033e2decaa
hefrot <hefrot@gmail.com>
2025-10-15 15:52:37 -0700

    Create calendar.js

---
+++ calendar.js
@@ -0,0 +1,19 @@
+{
+  "calendar": {
+    "round_1": [
+      { "white": "Jugador 1", "black": "Jugador 2" },
+      { "white": "Jugador 3", "black": "Jugador 4" },
+      ...
+    ],
+    "round_2": [
+      ...
+    ],
+    ...
+  },
+  "results": {
+    "round_1": {
+      "match_1": { "whitePoints": 9, "blackPoints": 0 },
+      ...
+    }
+  }
+}
