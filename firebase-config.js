137da9ae2117ee1c62c6546fe5cb41be63b36e59
hefrot <hefrot@gmail.com>
2025-10-15 16:06:06 -0700

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// firebase-config.js
+const firebaseConfig = {
+  apiKey: "AIzaSyC-qg8pD2OMjzOR5iwH_xIYVrwKlckCYGg",
+  authDomain: "hmena-torneo-bc215.firebaseapp.com",
+  databaseURL: "https://hmena-torneo-bc215-default-rtdb.firebaseio.com",
+  projectId: "hmena-torneo-bc215",
+  storageBucket: "hmena-torneo-bc215.firebasestorage.app",
+  messagingSenderId: "843984319871",
+  appId: "1:843984319871:web:3193a4c5d949b72616564a"
+};
